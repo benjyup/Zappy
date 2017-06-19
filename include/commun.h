662157a77a0df378e86551dfc212477cd1198d3c
Vincent Mesquita <vincent.mesquita@epitech.eu>
2017-06-19 18:13:43 +0200

    corerction commun.h

--- include/commun.h
+++ include/commun.h
@@ -5,7 +5,7 @@
 ** Login   <renard_e@epitech.net>
 ** 
 ** Started on  Mon Jun 12 09:20:07 2017 Gregoire Renard
-** Last update Mon Jun 19 17:56:25 2017 vincent.mesquita@epitech.eu
+** Last update Mon Jun 19 18:13:26 2017 vincent.mesquita@epitech.eu
 */
 
 #ifndef COMMUN_H_
@@ -17,26 +17,5 @@
 
 # define SUCCESS	1
 # define ERROR		-1
-
-typedef enum		e_bool
-  {
-    false = 0,
-    true
-  }			t_bool;
-
-typedef struct		s_list
-{
-  void		*data;
-  struct s_list	*prev;
-  struct s_list	*next;
-}			t_list;
-
-typedef struct		s_serv_env
-{
-
-}			t_serv_env;
-
-int			xdprintf(int fd,
-				 char *format, ...);
 
 #endif /* !COMMUN_H_ */
--- include/server.h
+++ include/server.h
@@ -5,12 +5,33 @@
 ** Login   <renard_e@epitech.net>
 ** 
 ** Started on  Mon Jun 12 09:19:50 2017 Gregoire Renard
-** Last update Mon Jun 12 09:21:30 2017 Gregoire Renard
+** Last update Mon Jun 19 18:13:31 2017 vincent.mesquita@epitech.eu
 */
 
 #ifndef SERVER_H_
 #define SERVER_H_
 
 #include "commun.h"
+
+typedef enum		e_bool
+  {
+    false = 0,
+    true
+  }			t_bool;
+
+typedef struct		s_list
+{
+  void		*data;
+  struct s_list	*prev;
+  struct s_list	*next;
+}			t_list;
+
+typedef struct		s_serv_env
+{
+
+}			t_serv_env;
+
+int			xdprintf(int fd,
+				 char *format, ...);
 
 #endif /* !SERVER_H_ */

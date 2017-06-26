e9f50d40e42a5f4dcddedc267ec5d848e1bde5bf
gregoire renard <gregoire.renard@epitech.eu>
2017-06-26 14:19:09 +0200

    know_type

--- src/server/src/know_team.c
+++ src/server/src/know_team.c
@@ -5,14 +5,14 @@
 ** Login   <renard_e@epitech.net>
 ** 
 ** Started on  Mon Jun 26 13:06:07 2017 Gregoire Renard
-** Last update Mon Jun 26 13:13:28 2017 Gregoire Renard
+** Last update Mon Jun 26 14:17:26 2017 Gregoire Renard
 */
 
 #include "server.h"
 
 void		know_team(t_env *env, t_client *client)
 {
-  if ((strcmp("GRAPHIC", client->split_cmd[0])) == 0)
+  if ((strcmp("GRAPHIC", client->cmd)) == 0)
     client->type = monitor;
   else
     add_to_the_team(env, client);
